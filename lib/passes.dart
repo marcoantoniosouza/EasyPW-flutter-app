491ac921e061631b12dc15abb28379ec6e808b5c
Marco Antonio Souza <maps@ic.ufal.br>
2020-07-03 02:44:18 -0300

    add the api call to get the passes list

--- lib/passes.dart
+++ lib/passes.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+import 'services/api.dart' as api;
 import 'env.dart' as env;
 
 class PassesPage extends StatefulWidget {
@@ -8,10 +11,29 @@ class PassesPage extends StatefulWidget {
 }
 
 class _PassesPageState extends State<PassesPage> {
+  final _passes = TextEditingController();
+
+  _apiGetPasses() async {
+    api.headers['auth'] = env.authHash;
+
+    await get(api.url + '/passes', headers: api.headers).then((res) {
+      setState(() {
+        print(res.body);
+        _passes.text = res.body;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _apiGetPasses();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(env.authHash),
+      body: Text(_passes.text),
     );
   }
 }

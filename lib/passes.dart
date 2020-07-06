5985380f093c42c93d7bc75c260d8b4e8167381c
Marco Antonio Souza <maps@ic.ufal.br>
2020-07-06 10:43:22 -0300

    Passes page style

--- lib/passes.dart
+++ lib/passes.dart
@@ -13,6 +13,17 @@ class _PassesPageState extends State<PassesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.teal),
+        title: Text(
+          'Senhas',
+          style: TextStyle(
+            color: Colors.teal,
+          ),
+        ),
+      ),
       body: FutureBuilder<List<PassController.Pass>>(
         future: PassController.getPasses(),
         builder: (context, snapshot) {
@@ -39,7 +50,53 @@ class PassesList extends StatelessWidget {
     return ListView.builder(
       itemCount: passes.length,
       itemBuilder: (context, index) {
-        return Text(passes[index].nome);
+        return Padding(
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                width: double.infinity,
+                height: 50,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.teal[200],
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.7,
+                          child: Text(
+                            passes[index].nome,
+                            style: TextStyle(
+                              color: Colors.grey[200],
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.visibility,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            print('Pressed');
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
       },
     );
   }
